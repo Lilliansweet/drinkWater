a461effb1198e27ce97fd1865cab625028119200
Sweety <1634093117@qq.com>
2023-05-24 08:17:07 +0800

    Initial Commit

---
+++ drinkWater/drinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  drinkWaterApp.swift
+//  drinkWater
+//
+//  Created by admin on 2023/5/24.
+//
+
+import SwiftUI
+
+@main
+struct drinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
